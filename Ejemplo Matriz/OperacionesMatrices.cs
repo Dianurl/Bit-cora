4019495718cae9882c834ca6ad66182059dab4f8
Dianurl <158301865+Dianurl@users.noreply.github.com>
2024-05-15 09:17:02 -0600

    Add files via upload

---
+++ Ejemplo Matriz/OperacionesMatrices.cs
@@ -0,0 +1,122 @@
+﻿
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+public class OperacionesMatrices
+{
+    public int[,] matriz = new int[0,0];
+
+    public OperacionesMatrices()
+    {
+    }
+
+    public void CrearMatriz()
+    {
+        int cantidadFilas = 0;
+        int cantidadCols = 0;
+
+        Console.WriteLine("Ingrese la cantidad de filas de la matriz");
+        cantidadFilas = Int32.Parse(Console.ReadLine() + "");
+        Console.WriteLine("Ingrese la cantidad de columnas de la matriz");
+        cantidadCols = Int32.Parse(Console.ReadLine() + "");
+
+        matriz = new int[cantidadFilas,cantidadCols];
+    }
+
+    public void IngresarDatosMatriz()
+    {
+        //Ejemplos con dos for (hay más formas)
+        for (int fila = 0; fila < matriz.GetLength(0); fila++)
+        {
+            for (int columna = 0; columna < matriz.GetLength(1); columna++)
+            {
+                Console.WriteLine($"Ingrese valor para la posicion [{fila}][{columna}]");
+                matriz[fila,columna] = Int32.Parse(Console.ReadLine() + "");
+            }
+        }
+    }  
+
+    public int[,] MultiplicaciónMatrizEscalar(int escalar)
+    {
+        int[,] matrizMultiplicada  = new int[matriz.GetLength(0), matriz.GetLength(1)];
+
+        //Ejemplos con dos for (hay más formas)
+        for (int fila = 0; fila < matriz.GetLength(0); fila++)
+        {
+            for (int columna = 0; columna < matriz.GetLength(1); columna++)
+            {
+                matrizMultiplicada[fila,columna] = matriz[fila,columna] * escalar;
+            }
+        }
+
+        return matrizMultiplicada;
+    }
+
+    public void ImprimirMatriz(int[,] matriz)
+    {
+        Console.WriteLine("La matriz es: ");
+        for (int fila = 0; fila < matriz.GetLength(0); fila++)
+        {
+            for (int columna = 0; columna < matriz.GetLength(1); columna++)
+            {
+                Console.Write(matriz[fila,columna] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+
+
+    public int[,] ValorMenor(int numeroencontrar)
+    {
+        int x = matriz.GetLength(0);
+        int[,] matrizvalor = new int[x, 2];
+
+        for(int fila = 0; fila < x; fila++)
+        {
+            int cont = 0;
+            for(int columna = 0; columna < matriz.GetLength(1); columna++)
+            {
+                if(matriz[fila,columna] < numeroencontrar)
+                {
+                    cont++;
+                }
+            }
+            matrizvalor[fila,0] = fila;
+            matrizvalor[fila,1] = cont;
+        }
+        return matrizvalor;
+    }
+
+    public int[,] Vectorpares()
+    {
+        int a = 0;
+
+        for (int i = 0; i < matriz.GetLength(0); i++)
+        {
+            for (int j = 0; j < matriz.GetLength(1); j++)
+            {
+                if(matriz[i,j] % 2 == 0)
+                {
+                    a++;
+                }
+            }
+        }
+
+        int[,] vec = new int[1, a];
+        int par = 0;
+
+        for (int i = 0; i < matriz.GetLength(0); i++)
+        {
+            for (int j = 0; j < matriz.GetLength(1); j++)
+            {
+                if(matriz[i,j] % 2 == 0)
+                {
+                    vec[0, par] = matriz[i,j];
+                    par++;
+                }
+            }
+        }
+
+        return vec;
+    }
+}
---
+++ Ejemplo Matriz/Program.cs
@@ -0,0 +1,49 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        OperacionesMatrices operacionesMatrices = new OperacionesMatrices();
+        char opcion = 'a';
+
+        operacionesMatrices.CrearMatriz();
+        operacionesMatrices.IngresarDatosMatriz();
+
+        while (opcion != 'd')
+        {
+            Console.WriteLine("Menú opciones");
+            Console.WriteLine(" a) Multiplicación de matriz por escalar");
+            Console.WriteLine(" b) Devolver todas las posisciones por fila de un elemento menor");
+            Console.WriteLine(" c) Encontrar vectores pares");
+            Console.WriteLine(" d) Salir");
+            Console.WriteLine("Ingrese opción: ");
+            opcion = Console.ReadLine()[0];
+
+            switch (opcion)
+            {
+                case 'a':
+                    Console.WriteLine("Ingrese un escalar");
+                    int escalar = Int32.Parse(Console.ReadLine() + "");
+                    int[,] matrizesca = operacionesMatrices.MultiplicaciónMatrizEscalar(escalar);
+                    operacionesMatrices.ImprimirMatriz(matrizesca);
+                    break;
+                
+                case 'b':
+                    int numeroencontrar;
+                    Console.WriteLine("Ingrese el numero que desea buscar");
+                    numeroencontrar = Int32.Parse(Console.ReadLine() + "");
+                    int[,] matrixfind = operacionesMatrices.ValorMenor(numeroencontrar);
+                    operacionesMatrices.ImprimirMatriz(matrixfind);
+                    break;
+
+                case 'c':
+                    int[,] matrixvec = operacionesMatrices.Vectorpares();
+                    operacionesMatrices.ImprimirMatriz(matrixvec);
+                    break;
+                
+                case 'd':
+                    Console.WriteLine("Se ha salido exitosamente");
+                    break;
+            }
+        }
+    }
+}
